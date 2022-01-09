980b29415a96bcdbaa21bba554706aca76babe93
pietro2356 <pietro.rocchio@gmail.com>
2022-01-09 10:48:18 +0100

    Aggiunto servizio messaggi e modifica eroi.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,22 +11,24 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   
-  constructor(private heroService: HeroService) { }
-
+  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  
 
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
 
 }
