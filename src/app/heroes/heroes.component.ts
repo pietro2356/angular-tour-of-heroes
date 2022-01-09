238263ced9e401242380b1c5af87d2f0681795bd
pietro2356 <pietro.rocchio@gmail.com>
2022-01-09 10:29:04 +0100

    Implementato servizio per caricamento dati.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +9,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
