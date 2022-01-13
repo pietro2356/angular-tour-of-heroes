164ad0f9301667544579b8d29af56884c00cf47e
pietro2356 <pietro.rocchio@gmail.com>
2022-01-13 19:42:19 +0100

    Extract the id route parameter and Add HeroService.getHero()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Tour Of Heroes tutorial';
+  np = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "E", "G", "X1", "X2", "X3", "X4", "X5"]
+  nd = [1124, 1197, 1275, 1358, 1446, 1540, 1640, 1747, 1860, 1981, 2110, 2247, 873, 930, 991, 1055, 2400]
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,7 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
@@ -25,10 +25,10 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
 
 }
